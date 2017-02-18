f4e2f28a327a6c4ce650e76665c101b33cf721d3
vikram-gsu <vpareddy1@student.gsu.edu>
2017-02-18 16:11:25 -0500

    test

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-document.querySelector('#nameInput').addEventListener('input', function(e){
+document.querySelector('#nameInput').addEventListener('keyup', function(e){
 	let name = e.target.value
 	let nameCount = 0
-	name.forEach(function(s){
-		nameCount += s
-	})
-	document.querySelector('#countLabel').value = nameCount
+	for (var i = name.length - 1; i >= 0; i--) {
+		nameCount += name.charCodeAt(i)
+	}
+	
+	document.querySelector('#countLabel').innerText = nameCount
 })
