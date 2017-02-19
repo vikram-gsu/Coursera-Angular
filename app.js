1797375d740d297e24c6b642fe8e73bb1668708f
vikram-gsu <vpareddy1@student.gsu.edu>
2017-02-19 13:11:18 -0500

    Assignment 1

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 (function(){
 	'use strict'
-	angular.module('DIApp', [])
+	angular.module('FoodApp', [])
 
-	.controller('DIController', DIController)
+	.controller('FoodController', FoodController)
 
-	DIController.$inject = ['$scope', '$filter']
-	function DIController($scope, $filter){
+	FoodController.$inject = ['$scope']
+	function FoodController($scope){
 		
-		$scope.name = 'Vikram'
-		var upCase = $filter('uppercase')
-
-		$scope.toUpCase = () => $scope.name = upCase($scope.name)
+		$scope.food = ''
+		$scope.checkiftoomuch = () => {
+			var foodItems = $scope.food.split(',')
+			
+			var numberOfItems = foodItems.filter(item => item.trim().length>0)
+																	.length
+			
+			if ($scope.food == ''){
+				$scope.response = "Please enter food first!"
+			}else if(numberOfItems >0 && numberOfItems <= 3){
+				$scope.response = 'Enjoy!'
+			}else{
+				$scope.response = 'Too much!'
+			}
+		}
 	}
 })()
